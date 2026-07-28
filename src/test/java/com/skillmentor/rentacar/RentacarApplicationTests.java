da9d1834c8a0861c241351e3e5a317b7b8c85800
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-07-28 13:54:30 +0530

    Initial commit

---
+++ src/test/java/com/skillmentor/rentacar/RentacarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillmentor.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
